678e06b225d8f71061f51f404afc06e919837d37
Chris Burger <chris@boop.li>
2020-12-21 20:49:59 -0600

    Adjust naming convention to match PEP8

--- fan_control.py
+++ fan_control.py
@@ -7,127 +7,127 @@ from bitstring import BitArray
 
 
 class FanController:
-    def __init__(self, dataPin):
+    def __init__(self, data_pin):
         # Setup output pin using BCM numbering (e.g. Pin22 = physical pin 15)
         GPIO.setmode(GPIO.BCM)
-        self.dataPin = dataPin
+        self.data_pin = data_pin
         
         # Set pin for output mode
-        GPIO.setup(self.dataPin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.data_pin, GPIO.OUT, initial=GPIO.LOW)
 
         # Setup mapping of command names to BitArrays
-        self.commandMap = {}
-        self.buildCommandMap()
+        self.command_map = {}
+        self._build_command_map()
 
 
-    def sleepMicrosecond(self, sleepTime):
+    def sleep_microsecond(self, sleep_time):
         # We use busy-waiting while transmitting to get +/- 100 microsecond accuracy on Linux
         # sleep() function isn't accurate enough with traditional OS/kernel
 
-        startTime = time.process_time()
-        doneWaiting = False
-        while not doneWaiting:
-            if time.process_time() >= startTime + (sleepTime/1000/1000):
-                doneWaiting = True
+        start_time = time.process_time()
+        done_waiting = False
+        while not done_waiting:
+            if time.process_time() >= start_time + (sleep_time/1000/1000):
+                done_waiting = True
 
-    def addressIntToBin(self, address: int):
+    def address_int_to_bin(self, address: int):
         if address > 15:
             return False
 
-        addressBinary = BitArray(uint=address, length=4)
+        address_binary = BitArray(uint=address, length=4)
         # All bits are sent inverted, so invert the address compared to the dip switches
-        addressBinary.invert()
-        return addressBinary
+        address_binary.invert()
+        return address_binary
 
-    def sendBit(self, bit):
+    def _send_bit(self, bit):
 
         # Assuming output pin is already low
 
         if bit:
             # 1 bit = 775 usec low
-            self.sleepMicrosecond(775)
+            self.sleep_microsecond(775)
         else:
             # 0 bit = 370 usec low
-            self.sleepMicrosecond(370)
+            self.sleep_microsecond(370)
 
         # Bring pin high
-        GPIO.output(self.dataPin, GPIO.HIGH)
+        GPIO.output(self.data_pin, GPIO.HIGH)
         
         if bit:
             # 1 bit = 370 usec high
-            self.sleepMicrosecond(370)
+            self.sleep_microsecond(370)
         else:
             # 0 bit = 775 usec high
-            self.sleepMicrosecond(775)
+            self.sleep_microsecond(775)
             
         # Bring pin back low
-        GPIO.output(self.dataPin, GPIO.LOW)
+        GPIO.output(self.data_pin, GPIO.LOW)
 
 
-    def sendCommand(self, address: int, command: str):
+    def send_command(self, address: int, command: str):
 
-        data = self.commandLookup(command)
+        data = self._command_lookup(command)
 
         # Invalid command provided
         if data == False:
             return False
 
-        addressBin = self.addressIntToBin(address)
+        address_bin = self.address_int_to_bin(address)
 
         # We will repeat each command multiple times to ensure reception
         for packet in range(3):
             # Send a one bit followed by a zero bit to signal start of packet
-            self.sendBit(1)
-            self.sendBit(0)
+            self._send_bit(1)
+            self._send_bit(0)
 
             # Next send 4-bit address
-            for bit in addressBin:
-                self.sendBit(bit)
+            for bit in address_bin:
+                self._send_bit(bit)
 
             # Now send a 1 bit, separating data from address info
-            self.sendBit(1)
+            self._send_bit(1)
 
             # Finally send 6 data bits with the command
             for bit in data:
-                self.sendBit(bit)
+                self._send_bit(bit)
 
             # Pause ~10ms between repeated packets.  sleep() is accurate enough for this
             time.sleep(10/1000)
 
-    def commandLookup(self, command):
+    def _command_lookup(self, command):
         try:
-            return self.commandMap[command]
+            return self.command_map[command]
         except KeyError:
             # Invalid command - not found
             return False
 
-    def buildCommandMap(self):
+    def _build_command_map(self):
         # Setup the mapping of command names to values
         # Command Bits are inverted for transmission, so a button press results in a zero bit in that position
-        self.commandMap["light"] = BitArray(bin="111110")
-        self.commandMap["fan_Off"] = BitArray(bin="111101")
-        self.commandMap["fan_Low"] = BitArray(bin="110111")
-        self.commandMap["fan_Med"] = BitArray(bin="101111")
-        self.commandMap["fan_Hi"] = BitArray(bin="011111")
+        self.command_map["light"] = BitArray(bin="111110")
+        self.command_map["fan_Off"] = BitArray(bin="111101")
+        self.command_map["fan_Low"] = BitArray(bin="110111")
+        self.command_map["fan_Med"] = BitArray(bin="101111")
+        self.command_map["fan_Hi"] = BitArray(bin="011111")
 
         
 
 if __name__ == "__main__":
     # BCM Pin 22 = Physical Pin 15
-    dataPin = input("BCM GPIO Pin Number for Output: ")
-    dataPin = int(dataPin)
-    control = FanController(dataPin)
+    data_pin = input("BCM GPIO Pin Number for Output: ")
+    data_pin = int(data_pin)
+    control = FanController(data_pin)
 
-    deviceAddress = input("Device Address [0-15]: ")
-    deviceAddress = int(deviceAddress)
+    device_address = input("Device Address [0-15]: ")
+    device_address = int(device_address)
 
     while True:
-        requestedCommand = input("Command to send or Q to quit? [Q/light/fan_Hi/fan_Med/fan_Low/fan_Off]: ")
+        requested_command = input("Command to send or Q to quit? [Q/light/fan_Hi/fan_Med/fan_Low/fan_Off]: ")
 
-        if requestedCommand.upper() == "Q":
+        if requested_command.upper() == "Q":
             raise SystemExit 
 
-        result = control.sendCommand(deviceAddress, requestedCommand)
+        result = control.send_command(device_address, requested_command)
         if result == False:
             print("Invalid Command")
 
